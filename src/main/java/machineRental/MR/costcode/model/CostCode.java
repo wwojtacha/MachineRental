365a0f269543864624abe9bad6c4a155c0bcdd57
WojciechWo <wwojtacha@o2.pl>
2020-10-07 19:56:02 +0200

    added new fields to CostCode model

--- src/main/java/machineRental/MR/costcode/model/CostCode.java
+++ src/main/java/machineRental/MR/costcode/model/CostCode.java
@@ -26,7 +26,13 @@ public class CostCode {
   @Column(nullable = false)
   private String costType;
 
+  @NotEmpty(message = "Project code description is required.")
+  @Column(nullable = false)
+  private String projectCodeDescription;
 
+  @NotEmpty(message = "Type of cost description is required.")
+  @Column(nullable = false)
+  private String costTypeDescription;
 
   @NotEmpty
   @Column(nullable = false, unique = true)
